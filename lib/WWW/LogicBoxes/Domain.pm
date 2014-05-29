1bf2050adc995dbd68d701817a5e56c63740d3f9
Robert Stone <drzigman@drzigman.com>
2014-05-28 22:18:33 -0500

    Fixed failing release tests regarding POD and SYNPOSIS
    In WWW::LogicBoxes::Domain
      Created POD
    
    In WWW::LogicBoxes::Role::Command::CheckAvailability
      Corrected syntax errors in SYNPOSIS
    
    In WWW::LogicBoxes::Role::Submit
      Created POD

--- lib/WWW/LogicBoxes/Domain.pm
+++ lib/WWW/LogicBoxes/Domain.pm
@@ -13,17 +13,42 @@ use Mozilla::PublicSuffix qw(public_suffix);
 # VERSION
 # ABSTRACT: LogicBoxes Domain Representation
 
+=head1 NAME
+
+WWW::LogicBoxes::Domain - LogicBoxes Representation of a Domain
+
+=head1 ATTRIBUTES
+
+=head2 name
+
+String representation of the domain name
+
+=cut
+
 has name => (
     is       => 'ro',
     isa      => 'Str',
     required => 1,
 );
 
+=head2 is_available
+
+Bool representating if this domain is available or not.
+
+=cut
+
 has is_available => (
     is  => 'ro',
     isa => 'Bool',
     builder => '_build_is_available',
 );
+
+=head2 tld
+
+The B<public suffix> of the domain in question.  This is an important distinction between
+the public suffix and the tld.  google.co.uk has a tld of "uk" and a public suffix of "co.uk"
+
+=cut
 
 sub tld {
     my $self = shift;
