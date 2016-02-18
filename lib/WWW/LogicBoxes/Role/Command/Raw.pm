1746dcbd46210e11dc5b4d1d4e83e7e162d2f568
Robert Stone <robertstone@hostgator.com>
2016-02-17 19:10:57 -0600

    Refactored Suggestion of Domains to use V5 of LogicBoxes Suggestions
    In WWW::LogicBoxes::Role::Command::Domain::Availability
      Refactored check_domain_availability and suggest_domains_names to use
      the v5 domain suggestion method of LogicBoxes
        This had to happen because LogicBoxes is removing the older way of
        doing it.
      Added deprecation warning for the hyphen argument
        This is no longer used
      Updated POD
    
    In WWW::LogicBoxes::Role::Command::Raw
      Added v5/suggest-names
    
    In WWW::LogicBoxes::Types
      Added Types: DomainAvailability and DomainAvailabilities
    
    In t/role/command/domain/availability/02-suggest_domains_names.t
      Refactored to check for the warning in the legacy route support
      V5 Suggest Names for Single TLD - With Related - 5 Results
      V5 Suggest Names for Single TLD - No Related - 5 Results
      V5 Suggest Names for Multiple TLD - With Related - 5 Results
      V5 Suggest Names for Multiple TLD - No Related - 5 Results

--- lib/WWW/LogicBoxes/Role/Command/Raw.pm
+++ lib/WWW/LogicBoxes/Role/Command/Raw.pm
@@ -19,11 +19,10 @@ requires 'username', 'password', 'api_key', '_base_uri', 'response_type';
 # ABSTRACT: Construct Methods For Making Raw LogicBoxes Requests
 
 use Readonly;
-# Create methods to support LogicBoxes API as of 2012-03-02
 Readonly our $API_METHODS => {
     domains => {
         GET => [
-            qw(available suggest-names validate-transfer search customer-default-ns orderid details details-by-name locks tel/cth-details)
+            qw(available suggest-names v5/suggest-names validate-transfer search customer-default-ns orderid details details-by-name locks tel/cth-details)
         ],
         POST => [
             qw(register transfer eu/transfer eu/trade uk/transfer renew modify-ns add-cns modify-cns-name modify-cns-ip delete-cns-ip modify-contact modify-privacy-protection modify-auth-code enable-theft-protection disable-theft-protection tel/modify-whois-pref resend-rfa uk/release cancel-transfer delete restore de/recheck-ns dotxxx/assoication-details)
